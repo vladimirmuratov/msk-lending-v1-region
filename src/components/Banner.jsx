48dffcf35c76d85fd6eb42df49dba67f0c0c8dc6
vladimir <vladimirmuratov@mail.ru>
2024-08-03 18:52:20 +0300

    first commit

---
+++ src/components/Banner.jsx
@@ -0,0 +1,75 @@
+'use client'
+
+import {Box, Button, Typography} from '@mui/material'
+import {phoneNumber} from '@/config'
+import {useEffect, useState} from 'react'
+import {useRouter} from 'next/navigation'
+
+export const Banner = () => {
+    const router = useRouter()
+    const [isMobile, setMobile] = useState(false)
+
+    useEffect(() => {
+        const os = navigator.userAgentData.platform
+
+        if (os === 'Android' || os === 'iOS') {
+            setMobile(true)
+        }
+
+    }, [])
+
+    return (
+        <Box className="banner-container" sx={{position: 'relative'}}>
+            <img className="banner" src="/images/banner3.jpg" alt="banner"/>
+            <Box sx={{
+                position: 'absolute',
+                top: {xs: '10%', sm: '20%'},
+                left: '5%',
+            }}>
+                <Typography
+                    variant="h4"
+                    sx={{
+                        fontSize: {xs: 32, sm: 58, md: 62},
+                        lineHeight: 1.1,
+                        fontWeight: 600,
+                        color: 'var(--black)',
+                        // position: 'absolute',
+                        // top: {xs: '10%', sm: '30%'},
+                        // left: '5%',
+                        textShadow: '0px 4px 4px lightgray',
+                        marginBottom: {xs: '5px', sm: '10px'}
+                    }}>
+                    Платная<br/>
+                    госпитализация из<br/>
+                    регионов
+                </Typography>
+
+                {isMobile
+                    ? <Button
+                        onClick={() => router.push(`tel:${phoneNumber}`)}
+                        variant="contained"
+                        color="error"
+                        size="large"
+                        sx={{
+                            // position: 'absolute',
+                            // top: {xs: '70%', sm: '80%', md: '70%', lg: '60%'},
+                            // left: '5%',
+                            display: isMobile ? 'block' : 'none'
+                        }}
+                    >
+                        Позвонить
+                    </Button>
+                    : <Typography sx={{
+                        color: 'var(--red)',
+                        // position: 'absolute',
+                        // top: '60%',
+                        // left: '5%',
+                        display: isMobile ? 'none' : 'block',
+                        fontSize: {xs: 16, sm: 32, md: 62},
+                        fontWeight: 600
+                    }}>{phoneNumber}</Typography>
+                }
+            </Box>
+        </Box>
+    )
+}
---
+++ src/components/MainInfoBlock.jsx
@@ -0,0 +1,117 @@
+import {Box, Typography} from '@mui/material'
+import CheckIcon from '@mui/icons-material/Check'
+
+export const MainInfoBlock = () => {
+    return (
+        <Box sx={{
+            scrollMarginTop: {xs: '120px', sm: '50px'},
+            paddingX: '32px',
+            paddingY: {xs: '25px', sm: '50px'},
+            color: 'var(--black)'
+        }}>
+
+            <Typography variant="h4" sx={{marginBottom: '20px', fontSize: {xs: '28px', sm: '34px'}, fontWeight: 500}}>
+                Ваше здоровье – наша забота: госпитализация в московскую больницу из регионов
+            </Typography>
+
+            <Typography component="p" sx={{fontSize: 20, marginBottom: '20px'}}>
+                Мы рады приветствовать вас в <Box component="span" sx={{fontWeight: 600}}>МСК-Групп</Box>, где ваше
+                здоровье и комфорт находятся в центре
+                внимания. Наша компания предоставляет высококачественное медицинское обслуживание не только жителям
+                столицы, но и пациентам из регионов России и СНГ. Мы делаем все возможное, чтобы ваша госпитализация
+                прошла
+                максимально удобно и безопасно.
+            </Typography>
+
+            <Typography component="p" sx={{fontSize: 20, marginBottom: '20px'}}>
+                <Box component="span" sx={{fontWeight: 600}}>МСК-Групп</Box> организует госпитализацию и транспортировку
+                из регионов и стран СНГ в клинику необходимого
+                профиля.
+                Организация встречи и помощь иногородним пациентам бригадой Скорой Помощи на вокзале или в аэропорту.
+            </Typography>
+
+            <Box sx={{display: 'flex', flexDirection: 'column', gap: '10px', marginBottom: '20px'}}>
+                <Box sx={{display: 'flex', alignItems: 'center', gap: '10px'}}>
+                    <CheckIcon sx={{color: 'var(--green)'}}/>
+                    <Typography component="p" sx={{fontSize: 20, fontWeight: 500}}>Высококвалифицированные
+                        специалисты</Typography>
+                </Box>
+                <Typography component="p" sx={{fontSize: 20}}>
+                    Наш медицинский персонал – это опытные врачи, медсестры и другие профессионалы, которые регулярно
+                    проходят обучение и повышение квалификации. Мы обеспечиваем вам доступ к лучшим медицинским кадрам
+                    страны.
+                </Typography>
+            </Box>
+            <Box sx={{display: 'flex', flexDirection: 'column', gap: '10px', marginBottom: '20px'}}>
+                <Box sx={{display: 'flex', alignItems: 'center', gap: '10px'}}>
+                    <CheckIcon sx={{color: 'var(--green)'}}/>
+                    <Typography component="p" sx={{fontSize: 20, fontWeight: 500}}>Современное оборудование</Typography>
+                </Box>
+                <Typography component="p" sx={{fontSize: 20}}>
+                    Мы используем новейшие медицинские технологии и оборудование, соответствующее мировым стандартам.
+                    Это позволяет нам проводить точные диагностику и лечение, обеспечивая наилучшие результаты для наших
+                    пациентов.
+                </Typography>
+            </Box>
+            <Box sx={{display: 'flex', flexDirection: 'column', gap: '10px', marginBottom: '20px'}}>
+                <Box sx={{display: 'flex', alignItems: 'center', gap: '10px'}}>
+                    <CheckIcon sx={{color: 'var(--green)'}}/>
+                    <Typography component="p" sx={{fontSize: 20, fontWeight: 500}}>Индивидуальный подход</Typography>
+                </Box>
+                <Typography component="p" sx={{fontSize: 20}}>
+                    Каждый пациент уникален, и мы разрабатываем персонализированные планы лечения, ориентированные на
+                    ваши конкретные потребности и состояние здоровья. Ваше здоровье – наш главный приоритет.
+                </Typography>
+            </Box>
+            <Box sx={{display: 'flex', flexDirection: 'column', gap: '10px', marginBottom: '20px'}}>
+                <Box sx={{display: 'flex', alignItems: 'center', gap: '10px'}}>
+                    <CheckIcon sx={{color: 'var(--green)'}}/>
+                    <Typography component="p" sx={{fontSize: 20, fontWeight: 500}}>Комфортные условия
+                        пребывания</Typography>
+                </Box>
+                <Typography component="p" sx={{fontSize: 20}}>
+                    Мы предлагаем уютные и удобные палаты, оборудованные всем необходимым для вашего комфорта. Мы
+                    создаем условия, способствующие быстрому восстановлению и вашему благополучию.
+                </Typography>
+            </Box>
+            <Box sx={{display: 'flex', flexDirection: 'column', gap: '10px', marginBottom: '20px'}}>
+                <Box sx={{display: 'flex', alignItems: 'center', gap: '10px'}}>
+                    <CheckIcon sx={{color: 'var(--green)'}}/>
+                    <Typography component="p" sx={{fontSize: 20, fontWeight: 500}}>Комплексный уход</Typography>
+                </Box>
+                <Typography component="p" sx={{fontSize: 20}}>
+                    От момента госпитализации до выписки мы обеспечиваем полный цикл медицинской помощи, включая
+                    реабилитацию и последующее наблюдение. Вы всегда можете рассчитывать на нашу поддержку.
+                </Typography>
+            </Box>
+            <Box sx={{display: 'flex', flexDirection: 'column', gap: '10px', marginBottom: '20px'}}>
+                <Box sx={{display: 'flex', alignItems: 'center', gap: '10px'}}>
+                    <CheckIcon sx={{color: 'var(--green)'}}/>
+                    <Typography component="p" sx={{fontSize: 20, fontWeight: 500}}>Поддержка в организации
+                        госпитализации</Typography>
+                </Box>
+                <Typography component="p" sx={{fontSize: 20}}>
+                    Мы помогаем вам организовать госпитализацию из любого региона России и СНГ, включая консультации,
+                    транспортировку и сопровождение. Наша цель – сделать ваш путь к здоровью как можно более гладким и
+                    беспрепятственным.
+                </Typography>
+            </Box>
+            <Box sx={{display: 'flex', flexDirection: 'column', gap: '10px', marginBottom: '20px'}}>
+                <Box sx={{display: 'flex', alignItems: 'center', gap: '10px'}}>
+                    <CheckIcon sx={{color: 'var(--green)'}}/>
+                    <Typography component="p" sx={{fontSize: 20, fontWeight: 500}}>Финансовая прозрачность</Typography>
+                </Box>
+                <Typography component="p" sx={{fontSize: 20}}>
+                    Мы предлагаем прозрачную систему оплаты и возможность получения консультаций по финансовым вопросам,
+                    чтобы вы могли планировать свои расходы на лечение.
+                </Typography>
+            </Box>
+
+            <Typography component="p" sx={{fontSize: 20}}>
+                Выберите <Box component="span" sx={{fontWeight: 600}}>МСК-Групп</Box> для вашего здоровья и долголетия. Доверьте ваше здоровье профессионалам,
+                которые готовы заботиться о вас круглосуточно. Мы – ваш надежный партнер в борьбе за здоровье и качество
+                жизни, независимо от того, где вы находитесь.
+            </Typography>
+        </Box>
+    )
+}
