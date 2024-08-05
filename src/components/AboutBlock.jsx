d72d2c6c2222195748c4d192699458eb1bfb0ca8
vladimir <vladimirmuratov@mail.ru>
2024-08-05 09:56:20 +0300

    update content

--- src/components/AboutBlock.jsx
+++ src/components/AboutBlock.jsx
@@ -16,15 +16,21 @@ export const AboutBlock = () => {
             <Typography variant="h4" sx={{marginBottom: '20px', fontSize: {xs: '28px', sm: '34px'}, fontWeight: 500}}>О
                 нас</Typography>
 
-            <Typography component="p" sx={{fontSize: 20, marginBottom: '10px'}}>
+            <Typography
+                component="p"
+                sx={{
+                    fontSize: 20,
+                    // marginBottom: '10px'
+                }}
+            >
                 Московская Сервисная Компания (МСК-Групп) уже 10 лет оказывает услуги платной госпитализации и
                 транспортировки
                 пациентов в ведущие клиники Москвы в круглосуточном режиме.
             </Typography>
-            <Typography component="p" sx={{fontSize: 20}}>
+            {/*<Typography component="p" sx={{fontSize: 20}}>
                 Наша компания работает на базе ФГБУ "Объединенная больница с поликлиникой" Управления Делами Президента
                 РФ.
-            </Typography>
+            </Typography>*/}
         </Box>
     )
 }
