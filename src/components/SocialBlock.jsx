a49eb3379bef693f2067d585516cd69f580a1937
vladimir <vladimirmuratov@mail.ru>
2026-02-12 16:22:05 +0300

    update massages icons

--- src/components/SocialBlock.jsx
+++ src/components/SocialBlock.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import {Box, Link} from '@mui/material'
-import Image from 'next/image'
-import { email, phoneTelegram, phoneViber, phoneWhatsApp } from '@/config';
+import React from 'react';
+import { Box, Link } from '@mui/material';
+import Image from 'next/image';
+import { email, maxLink, telegramLink } from '@/config';
 
-const SocialBlock = ({className = ''}) => {
+const SocialBlock = ({ className = '' }) => {
     return (
         <Box
             sx={{
@@ -14,15 +14,41 @@ const SocialBlock = ({className = ''}) => {
             }}
             className={className}
         >
-            <Link target="_blank" href={`https://wa.me/${phoneWhatsApp}`}>
+            {/*<Link target="_blank" href={`https://wa.me/${phoneWhatsApp}`}>
                 <Image
                     src="/images/social/whatsapp.svg"
                     alt="icon"
                     width={30}
                     height={30}
                 />
+            </Link>*/}
+            <Link
+                target="_blank"
+                href={maxLink}
+                sx={{
+                    transition: 'all 0.3s',
+                    ':hover': {
+                        transform: 'scale(1.1)',
+                    }
+                }}
+            >
+                <Image
+                    src="/images/social/max.svg"
+                    alt="icon"
+                    width={30}
+                    height={30}
+                />
             </Link>
-            <Link target="_blank" href={`https://t.me/${phoneTelegram}`}>
+            <Link
+                target="_blank"
+                href={telegramLink}
+                sx={{
+                    transition: 'all 0.3s',
+                    ':hover': {
+                        transform: 'scale(1.1)',
+                    }
+                }}
+            >
                 <Image
                     src="/images/social/telegram.svg"
                     alt="icon"
@@ -38,7 +64,15 @@ const SocialBlock = ({className = ''}) => {
                     height={30}
                 />
             </Link>*/}
-            <Link href={`mailto:${email}`}>
+            <Link
+                href={`mailto:${email}`}
+                sx={{
+                    transition: 'all 0.3s',
+                    ':hover': {
+                        transform: 'scale(1.1)',
+                    }
+                }}
+            >
                 <Image
                     src="/images/social/email.svg"
                     alt="icon"
@@ -47,7 +81,7 @@ const SocialBlock = ({className = ''}) => {
                 />
             </Link>
         </Box>
-    )
-}
+    );
+};
 
-export default React.memo(SocialBlock)
+export default React.memo(SocialBlock);
